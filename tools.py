63cbd8b189ed6f7366a19efc9504289cd352d245
JoBe <jolego879@outlook.de>
2025-05-18 10:50:19 +0200

    Updated some small stuff!

--- tools.py
+++ tools.py
@@ -11,21 +11,25 @@ for file in Path(".").rglob("main.md"):
 
 
 if not __name__ == "__main__": 
-    exit("Please don't try to import tools.py")
-    
+    raise ImportError("Please don't try to import tools.py")
+
 print("Please input your file, with the file extension")
 user_input = input()
 try:
     file_name = user_input.split(".")[0]
     file_extension = user_input.split(".")[1]
-except IndexError: raise Exception("Please Input a file with the file extension following after a \".\", e.g.: \"Example.md\"")
+except IndexError: 
+    raise Exception("Please Input a file with the file extension following after a \".\", e.g.: \"Example.md\"")
 
 if file_extension ==  "md": 
     from Markdown_to_HTML import *
     main(user_input)
     print(rf"Generated file at output\{file_name}.html")
+
 elif file_extension in ("htm", "html"): 
     from HTML_to_Markdown import *
     main(user_input)
     print(rf"Generated file at output\{file_name}.md")
-else: raise Exception("Can only accept .md, .htm and .html files")
+
+else: 
+    raise Exception("Can only accept .md, .htm and .html files")
